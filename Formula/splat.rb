cca0553fbad890a7696835f004441e3dbb61a72d
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-03 20:23:29 -0400

    Brew formula update for splat version v0.4.3-pre13

--- Formula/splat.rb
+++ Formula/splat.rb
@@ -5,29 +5,29 @@
 class Splat < Formula
   desc ""
   homepage ""
-  version "0.4.3-pre12"
+  version "0.4.3-pre13"
   license "AGPL"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre12/splat_0.4.3-pre12_darwin_amd64.tar.gz"
-      sha256 "6751fbb8598d77ab5268a021ef91e69066b68f3714a71b2673502f116e342eba"
+      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre13/splat_0.4.3-pre13_darwin_amd64.tar.gz"
+      sha256 "7c4a105f4b3d53046c8261282063e34ddea45ceadd8fa583141331bf01e373ca"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre12/splat_0.4.3-pre12_darwin_arm64.tar.gz"
-      sha256 "599c053a6acdfc8a606db80ea597cb8c48c7020fe8aade407f6c34016a30ad80"
+      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre13/splat_0.4.3-pre13_darwin_arm64.tar.gz"
+      sha256 "5e3eb6c61721d9f44658a87291687951c743472ee4b78c94798b8440571db0cb"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre12/splat_0.4.3-pre12_linux_amd64.tar.gz"
-      sha256 "68622728c409b0f6e5808929ef39a9862d4f1fdb1f45b2bc928f56f1ad1cd996"
+      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre13/splat_0.4.3-pre13_linux_amd64.tar.gz"
+      sha256 "760d11a78f21fd922628320d7f2a79de250613e1b00aebe8a6288381fc29b2a9"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre12/splat_0.4.3-pre12_linux_arm64.tar.gz"
-      sha256 "3631b46bc9a97e1bf890acdb1a6c427f7c1ecbd47f6fe7f79406c540270c5806"
+      url "https://github.com/YaleSpinup/splat/releases/download/v0.4.3-pre13/splat_0.4.3-pre13_linux_arm64.tar.gz"
+      sha256 "f5cf80f270ee2314e93712180ec8ae65734a1982de0f5a26d4c31285a38b2b60"
     end
   end
 
