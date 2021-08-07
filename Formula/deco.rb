e6949d3fb1bf9af1e31ebc1ba313deae911194e9
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-07 07:34:15 -0400

    Brew formula update for deco version v1.1.0

--- Formula/deco.rb
+++ Formula/deco.rb
@@ -5,29 +5,29 @@
 class Deco < Formula
   desc "DockerEvironmentCOntrol"
   homepage "https://github.com/YaleUniversity/deco"
-  version "1.1.0-pre02"
+  version "1.1.0"
   license "AGPL-3.0-or-later"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0-pre02/deco_1.1.0-pre02_darwin_amd64.tar.gz"
-      sha256 "6ad015a9de77ac0b9a162f1e1ab6038397cfe580a7f783bd4c65a24f856568e8"
+      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0/deco_1.1.0_darwin_amd64.tar.gz"
+      sha256 "6f485ffad9e0cd5cfb3c20c3724fa5dada94516a0118ae46963481029c35ad16"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0-pre02/deco_1.1.0-pre02_darwin_arm64.tar.gz"
-      sha256 "1f855785cb18bc458b9fc52577b3aa67bc671340940499723afd60bc56392bae"
+      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0/deco_1.1.0_darwin_arm64.tar.gz"
+      sha256 "6769833895b98546bedf7fb3d951bef3e8c844529b9bceecb1bbd087704e582c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0-pre02/deco_1.1.0-pre02_linux_amd64.tar.gz"
-      sha256 "1ced626510f4094c1e67e1a05bce17345d2effc22164874b44c2bc7d0305d776"
+      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0/deco_1.1.0_linux_amd64.tar.gz"
+      sha256 "9ed7cdbab12d122b266f8478f010ebf9c4bfa091f7613db46349d4f74d1e7f6b"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0-pre02/deco_1.1.0-pre02_linux_arm64.tar.gz"
-      sha256 "c7fb2e912dcec4c3689da49d0453c12d1c55ede3f03b488e9447e3271966a3d3"
+      url "https://github.com/YaleUniversity/deco/releases/download/v1.1.0/deco_1.1.0_linux_arm64.tar.gz"
+      sha256 "68b5272c0d5ccc16297f8b3010e926ef40dd80cf5934653e90234b5086fcb79e"
     end
   end
 
