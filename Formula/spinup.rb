3a0cda4ef93aebfc742e7d5b5c58f1ca54901b58
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-02 11:25:07 -0400

    Brew formula update for spinup-cli version v0.4.0-pre05

--- Formula/spinup.rb
+++ Formula/spinup.rb
@@ -5,29 +5,29 @@
 class Spinup < Formula
   desc "Simple command line interface for spinup"
   homepage "https://github.com/YaleSpinup/spinup-cli"
-  version "0.4.0-pre03"
+  version "0.4.0-pre05"
   license "AGPL-3.0-or-later"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre03/spinup-cli_0.4.0-pre03_darwin_amd64.tar.gz"
-      sha256 "a306efacd98d1575784f75bd4c69cbc71f509a56d03184c26b8c72bed041bd79"
+      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre05/spinup-cli_0.4.0-pre05_darwin_amd64.tar.gz"
+      sha256 "98295d04fcf47116021c0d99c6f03a7acad58a3d30b67dc90987c1e535740dd4"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre03/spinup-cli_0.4.0-pre03_darwin_arm64.tar.gz"
-      sha256 "f2533d489e7b78e9dd8dced417756f6e7e44857bcd67d74f748f5e943d7b19b2"
+      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre05/spinup-cli_0.4.0-pre05_darwin_arm64.tar.gz"
+      sha256 "c8a0e370e17cb008ea6fd31018a93a5eeb4c61dae80f09d96e1fea438ad47dd0"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre03/spinup-cli_0.4.0-pre03_linux_amd64.tar.gz"
-      sha256 "a07578eb03ed2838a4630b71b2ce764b6f0bac3f4238ec1e4ddfd19c90c269be"
+      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre05/spinup-cli_0.4.0-pre05_linux_amd64.tar.gz"
+      sha256 "8824b82b664243f34596c9bd74821a24318105d828344f868dd984c88d1ce8ad"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre03/spinup-cli_0.4.0-pre03_linux_arm64.tar.gz"
-      sha256 "9a45d78c4c0e05f8b1d5a6b855fe16b5909f55dbab2b6462921e2db9d70c0943"
+      url "https://github.com/YaleSpinup/spinup-cli/releases/download/v0.4.0-pre05/spinup-cli_0.4.0-pre05_linux_arm64.tar.gz"
+      sha256 "e45a27ead65539413b745d6d7da8437656ed60f877dc35f7c2e1ed3b301d7db0"
     end
   end
 
